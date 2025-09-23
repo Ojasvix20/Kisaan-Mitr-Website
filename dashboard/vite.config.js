52fd2e3dc39df116be984b102f13c9bf24e73179
Ojasvi Bakshi <ojasvi.bakshi@gmail.com>
2025-09-23 17:09:09 +0530

    update config files

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Kisaan-Mitr-Website/', // <-- This must match your repo name exactly!
+  base: 'Kisaan-Mitr-Website', // <-- This must match your repo name exactly!
   plugins: [react()],
 })
