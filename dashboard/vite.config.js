d2b60e7c1b57bd2ffcffe4f5df156484bdebcac7
Ojasvi Bakshi <ojasvi.bakshi@gmail.com>
2026-07-30 01:24:55 +0530

    clear

--- dashboard/vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: 'Kisaan-Mitr-Website', // <-- This must match your repo name exactly!
-  plugins: [react()],
-})
