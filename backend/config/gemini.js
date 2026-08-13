04c979a078c99a0ca4bbe1a1e97759df74b6e5a1
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2026-08-13 12:34:31 +0530

    Update gemini.js

--- backend/config/gemini.js
+++ backend/config/gemini.js
@@ -1,9 +1,10 @@
+import "dotenv/config";
 import { GoogleGenAI } from "@google/genai";
 
 const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY,
   httpOptions: {
-    timeout: 30000,
+    timeout: 60000,
   },
 });
 
